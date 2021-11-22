034d2194739961d56e24a4ca408d981edecbcb9b
hoba-android <heba96@yahoo.com>
2021-11-22 21:51:24 +0200

    css

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector('#menu').addEventListener('click', () => {
+  document.querySelector('nav ul').classList.toggle('showmenu');
+});
+
+document.querySelector('#men').addEventListener('click', () => {
+  document.querySelector('nav ul').classList.toggle('showmenu');
+});
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(`.row`, {
+  interval: 200,
+});
