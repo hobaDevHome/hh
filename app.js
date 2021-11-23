ea4dc8cbac8c93161ccc0218675b56156af6540e
hoba-android <heba96@yahoo.com>
2021-11-23 18:26:36 +0200

    nav

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 document.querySelector('#menu').addEventListener('click', () => {
-    console.log('clicked')
+  console.log('clicked');
   document.querySelector('nav').classList.toggle('showmenu');
 });
 
-// document.querySelector('#men').addEventListener('click', () => {
-//   document.querySelector('nav ul').classList.toggle('showmenu');
-// });
+document.querySelector('#men').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('showmenu');
+});
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 800,
-    reset: true
+  origin: 'top',
+  distance: '30px',
+  duration: 800,
+  reset: true,
 });
 
 sr.reveal(`.animate`, {
-    interval: 200
-})
+  interval: 200,
+});
 
-/*==================== SHOW SCROLL TOP ====================*/ 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+  else scrollTop.classList.remove('show-scroll');
 }
-window.addEventListener('scroll', scrollTop)
-
+window.addEventListener('scroll', scrollTop);
